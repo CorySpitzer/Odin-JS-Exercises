e0cbfd7b8e3020cc29e5d5bf48aa5775e180da57
Cory Spitzer <coryspitzer@gmail.com>
2024-11-14 14:22:21 -0500

    Repeat strings x times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+// function checkIfNeg (number) {
+//   if (number < 0) {
+//     throw new Error("times cannot be negative!");
+//   }
+// }
 
+const repeatString = function(strToRepeat, times) {
+  // try {
+  //   checkIfNeg(times)
+  // } catch (error) {
+  //   // throw new Error("times cannot be negative!");
+  // }
+  if (times < 0) {
+    return "ERROR";
+  }
+  return strToRepeat.repeat(times); 
 };
 
 // Do not edit below this line
