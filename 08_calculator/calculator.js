703fe5d7222b53f43eef55cf2e542cb9894e2a56
Cory Spitzer <coryspitzer@gmail.com>
2024-11-15 17:07:22 -0500

    Sum and Multiply numbers in arrays

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,19 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce( function (a, b) {
+    return a + b;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce( function (a, b) {return a * b}, 1)
 };
 
 const power = function() {
