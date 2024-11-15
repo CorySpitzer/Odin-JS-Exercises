1297e9c2079b857bacb11ad1853896d530a215a1
Cory Spitzer <coryspitzer@gmail.com>
2024-11-15 18:50:30 -0500

    Test for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // 2nd replace removes whitespace
+  let noPunctStr = str.replace(/[^\w\s\']|_/g, "")
+                      .replace(/\s+/g, '')
+                      .toLowerCase();;
+  
+  let reversedStr = noPunctStr.split('').reverse().join('')
+                              .toLowerCase();
 
+  return reversedStr == noPunctStr.toLowerCase();
 };
+
+// palindromes('r3ace3car');
 
 // Do not edit below this line
 module.exports = palindromes;
