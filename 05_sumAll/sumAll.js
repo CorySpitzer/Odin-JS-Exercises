cfa8ff81e0f03f7588e251917933d02e447daccb
Cory Spitzer <coryspitzer@gmail.com>
2024-11-15 02:30:04 -0500

    Sums numbers within a range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+  let sum = 0;
+  for (let i = int1; i <= int2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// 5 6 7 8
