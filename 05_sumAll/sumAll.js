94f5da5b55aa1555be51a4ac1b5839ed7e98ce98
Cory Spitzer <coryspitzer@gmail.com>
2024-11-15 02:46:04 -0500

    Works with large numbers first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,22 @@
 const sumAll = function(int1, int2) {
+  if (int1 > int2) {
+    let swap = int1;
+    int1 = int2;
+    int2 = swap;
+  }
   let sum = 0;
   for (let i = int1; i <= int2; i++) {
     sum += i;
   }
   return sum;
 };
+// const sumAll = function(int1, int2) {
+//   let sum = BigInt(0);
+//   for (let i = BigInt(int1); i <= BigInt(int2); i++) {
+//     sum += i;
+//   }
+//   return Number(sum);
+// };
 
 // Do not edit below this line
 module.exports = sumAll;
