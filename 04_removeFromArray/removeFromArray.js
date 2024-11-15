b3bd4b117bdae0f76abd319eae3f16983584b344
Cory Spitzer <coryspitzer@gmail.com>
2024-11-15 02:14:39 -0500

    Removes multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,15 @@
-const removeFromArray = function(inputArray, elementToRemove) {
+const removeFromArray = function(inputArray, ...elementsToRemove) {
   let outputArray = [];
   for (let i = 0; i < inputArray.length; i++) {
-    if (inputArray[i] != elementToRemove) {
+    if (!(elementsToRemove.includes((inputArray[i])))) {
       outputArray.push(inputArray[i]);
     }
   }
+
+  // loop over input array
+  // if value not in elementsToRemove
+  // add to output array
+
   return outputArray;
 };
 
