e4f2b582287ac28b3ff762368dfaff2b8611cf7c
Cory Spitzer <coryspitzer@gmail.com>
2024-11-15 01:52:39 -0500

    Removes a single value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, elementToRemove) {
+  let outputArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] != elementToRemove) {
+      outputArray.push(inputArray[i]);
+    }
+  }
+  return outputArray;
 };
 
 // Do not edit below this line
