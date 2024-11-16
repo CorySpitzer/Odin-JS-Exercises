b58c4fbe9ef9cfcc87e02f63ad9b7cb81f1e6c9a
Cory Spitzer <coryspitzer@gmail.com>
2024-11-16 04:01:13 -0500

    Finds the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldestAge = 0;
+  let age = 0;
+  let oldestPerson = people[0].name;
+  console.log("OP: " + people[0].name);
+  for (let i = 0; i < people.length; i++) {
+    age = people[i].yearOfDeath - people[i].yearOfBirth
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = people[i];
+      console.log("OP " + people[i].name);
+    }
+  } 
+  return oldestPerson;
 };
 
 // Do not edit below this line
